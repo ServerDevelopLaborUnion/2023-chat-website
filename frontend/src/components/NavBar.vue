a02ef2d91c92cf7d4c765eeefe333f6ff293d2e8
kwak1s1h <kwak1s1h@kakao.com>
2023-09-07 20:27:16 +0900

    vue 프론트엔드

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <!-- 로고 또는 웹사이트 이름 -->
+        <router-link to="/" class="navbar-brand">My Chat App</router-link>
+  
+        <!-- 네비게이션 메뉴 -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link to="/home" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NavBar',
+  props: {
+    msg: String,
+  },
+});
+</script>
